1de8dff4cd7cdd170a69b023fece05d723ef8dfd
artem.tensin <artem.tensin@kupibilet.ru>
2020-03-28 23:02:39 +0300

    * added demonstration

--- lib/model/Config.dart
+++ lib/model/Config.dart
@@ -1,5 +1,5 @@
 const int BASE_ANIMATION_DURATION = 5;
 const int FISH_COUNT = 10;
-const double FISH_BASE_WIDTH = 30.0;
-const double FISH_BASE_HEIGHT = 45.0;
+const double FISH_BASE_WIDTH = 20.0;
+const double FISH_BASE_HEIGHT = 35.0;
 const int ADD_NEW_FISH_TIMER = 15;
